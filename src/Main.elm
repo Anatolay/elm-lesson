60cc15909c65cfafa9689d3b96c1d13e53a5dc62
Anatolay <nihonvasserman228@gmail.com>
2023-03-15 18:03:50 +0300

    Add homework

--- src/Main.elm
+++ src/Main.elm
@@ -9,44 +9,67 @@ import Html.Events exposing (onClick)
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = 0
+        { init = List.repeat 5 0
         , view = view
         , update = update
         }
 
 
+
+-- Your Model is a list of integers now. It represents
+-- an arbitrary number of counters to display to the user.
+--
+-- Your goal is to write a view and update functions that
+-- deal with lists of arbitrary length.
 type alias Model =
-    Int
+    List Int
 
 
+
+-- TODO: Finish the message data type.
+-- It should contain information on which counter is affected
+-- and how the counter is affected (incremented/decremented).
+--
+-- bonus: add an extra action to reset the counter.
 type Msg
-    = Incr
-    | Decr
+    = MyMsg -- ??? ???
 
 
+
+-- TODO: should display all counters from the list
 view : Model -> Html.Html Msg
-view x =
-    -- If you think of it, each HTML element consists of 3 parts:
-    -- element name (1), a list of attributes (2) and a list of child elements (3).
-    --
-    -- Thus, the Html package provides all elements as functions where
-    -- function name represents attribute name (1) and each such function takes
-    -- two lists as arguments: the first one for the attributes (2)
-    -- and the second for child elements (3).
+view _ =
     Html.div [ class "container" ]
-        [ Html.div []
-            [ Html.button [ onClick Decr ] [ Html.text "-" ]
-            , Html.text (String.fromInt x)
-            , Html.button [ onClick Incr ] [ Html.text "+" ]
-            ]
+        [ Html.text "Hi!" ]
+
+
+-- TODO: should display a single counter.
+-- Fist argument is counter value.
+-- Second argument is left for you to define. It should provide
+-- a way to fire the appropriate message.
+--
+-- bonus: add an extra button to fire a 'reset counter' message.
+viewCounter : Int -> () -> Html.Html Msg
+viewCounter val counter =
+    let
+        -- [ onClick decrementCounter ], based on `counter` argument
+        decrAttrs =
+            [ onClick MyMsg ]
+        -- [ onClick incrementCounter ], based on `counter` argument
+        incrAttrs =
+            [ onClick MyMsg ]
+    in
+    Html.div []
+        [ Html.button decrAttrs [ Html.text "-" ]
+        , Html.text (String.fromInt val)
+        , Html.button incrAttrs [ Html.text "+" ]
         ]
 
 
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Decr ->
-            model - 1
 
-        Incr ->
-            model + 1
+-- TODO: implement.
+-- Fired message should contain information on which counter should be updated,
+-- as well as how it should be updated. Update that counter.
+update : Msg -> Model -> Model
+update _ model =
+    model
